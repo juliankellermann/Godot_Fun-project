8efb1092facad1ae907ebb6b1a9942fe7ba08b92
Julian Kellermann <julian.kellermann@stud.th-deg.de>
2026-05-20 01:03:59 +0200

    Modularisierung von _physics_process in BallPhysics.gd zur Verbesserung der Lesbarkeit

--- scripts/BallPhysics.gd
+++ scripts/BallPhysics.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 
 
 const AUFTRIEB_BASIS = -45.0
+const START_Y_OFFSET = -25
 var kanten_abstand: float 
 var ist_am_start = true
 var paddle 
@@ -17,48 +18,68 @@ func _ready():
 	paddle = get_parent().get_node("Paddle")
 
 	if paddle:
-		
 		var color_rect = paddle.get_node("ColorRect")
 		kanten_abstand = color_rect.size.x / 4 
 
 
 ## Berechnet die Bewegung des Balls und löst Kollisionen in jedem Frame auf.
-## Enthält modifizierte Abprall-Logik für das Paddle: Trifft der Ball das erste
-## oder Letzte viertel des Paddles wird ein anderer Winkel abhängig vom Trefferpunkt berechnet
-func _physics_process(delta: float):
+func _physics_process(delta: float) -> void:
 	if ist_am_start:
-		
-		if paddle:
-			global_position = paddle.global_position + Vector2(0, -25)
-			
-		if Input.is_action_just_pressed("ui_accept"):
-			abschiessen()
+		_handle_start_state()
 	else:
-		var collision = move_and_collide(velocity * delta)
+		_handle_movement(delta)
+
+
+func _handle_start_state() -> void:
+	if paddle: 
+		global_position = paddle.global_position + Vector2(0, START_Y_OFFSET)
 		
-		if collision:
-			var collider = collision.get_collider()
-			var bounce_default = true # Standardmäßig normal abprallen
-			
-			if collider.name == "Paddle":
-				var pos = global_position.x - collider.global_position.x
-				
-				# Am Rand des Paddles mit speziellem Winkel abprallen, um 
-				# dem Spieler gezielte Schüsse zu ermöglichen.
-				if pos >= kanten_abstand or pos <= -kanten_abstand:
-					
-					# pos bestimmt die X-Richtung, AUFTRIEB_BASIS zwingt den Ball nach oben.
-					# .normalized() wandelt dieses Verhältnis in den finalen Winkel um.
-					var tempSPD = Vector2(pos, AUFTRIEB_BASIS)
-					velocity = tempSPD.normalized() * speed
-					
-					bounce_default = false # Die Standard-Physik überschreiben
-					
-			if bounce_default:
-				if collider.has_method("treffer"):
-					collider.treffer()
-				# Normaler physikalischer Abprall an Wänden und Blöcken
-				velocity = velocity.bounce(collision.get_normal())
+	if Input.is_action_just_pressed("ui_accept"):
+		abschiessen()
+
+
+func _handle_movement(delta: float) -> void:
+	var collision = move_and_collide(velocity * delta)
+	
+	if not collision:
+		return
+		
+	_resolve_collision(collision)
+
+
+func _resolve_collision(collision: KinematicCollision2D) -> void:
+	var collider = collision.get_collider()
+	
+	if _try_paddle_bounce(collider):
+		return
+		
+	_apply_standard_bounce(collision)
+
+
+## Prüft, ob der Ball das Paddle an den Rändern trifft. 
+## Gibt true zurück, wenn die Flugbahn modifiziert wurde.
+func _try_paddle_bounce(collider: Object) -> bool:
+	if collider.name != "Paddle":
+		return false
+		
+	var hit_offset_x: float = global_position.x - collider.global_position.x
+	
+	if abs(hit_offset_x) >= kanten_abstand:
+		var bounce_direction := Vector2(hit_offset_x, AUFTRIEB_BASIS).normalized()
+		velocity = bounce_direction * speed
+		return true
+		
+	return false
+
+
+## Führt den normalen physikalischen Abprall und Treffer-Logik aus.
+func _apply_standard_bounce(collision: KinematicCollision2D) -> void:
+	var collider = collision.get_collider()
+	
+	if collider.has_method("treffer"):
+		collider.treffer()
+		
+	velocity = velocity.bounce(collision.get_normal())
 
 func abschiessen():
 	ist_am_start = false
